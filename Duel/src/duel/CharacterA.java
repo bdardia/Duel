601b1da3c699e0205f91ae6e06794c4db2d0fd36
BT_1N3_04 <BT_1N3_04@BT_1N3_04.bktech.local>
2017-09-15 15:33:53 -0400

    updated CharacterA

--- Duel/src/duel/CharacterA.java
+++ Duel/src/duel/CharacterA.java
@@ -4,7 +4,11 @@ import java.util.Random;
 
 public class CharacterA implements Dueler{
 	private static final String[] TAUNTS = {"Im amazing", " get rekted"};
+	private final int attacking = 1;
+	private final int defending = 0;
 	private int hp = -1;
+	
+	private Random rand = new Random();
 	
 	
 	private String name = "Player_3";
@@ -33,10 +37,33 @@ public class CharacterA implements Dueler{
 	}
 	
 	public void taunt() {
-		Random rand = new Random();
-		String taunt = TAUNTS[rand.nextInt(TAUNTS.length)];
+		String taunt = TAUNTS[this.rand.nextInt(TAUNTS.length)];
 		System.out.println(taunt);
 
+	}
+	
+	public int getAction(Object caller) {
+		
+		if(caller instanceof Duel) {
+			int descision = this.rand.nextInt(1);
+			if(descision == this.defending) {
+				return 2;
+			}
+			else if(descision == this.attacking){
+				if (caller.d1Loaded) {
+					return 1;
+				}
+				else {
+					return 0;
+				}
+			}
+		}
+		return 3;
+	}
+		
+		
+	public void hit(Object caller) {
+		
 	}
 
 	
